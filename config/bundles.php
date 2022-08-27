15808a07c723713623868ef2b2ca7f469f8d8f35
Lenny4 <alexandre.beaujour@hotmail.fr>
2022-08-26 21:53:44 -0400

    almost create docker

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['dev' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['dev' => true, 'all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
